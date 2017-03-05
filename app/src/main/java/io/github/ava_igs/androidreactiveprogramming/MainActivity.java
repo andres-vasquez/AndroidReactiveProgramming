ef7fbc2e645104f81f54e24a14ac11c247629d3c
Andres Vasquez <andres.vasquez.a@hotmail.com>
2017-03-05 14:43:44 -0400

    First commit

---
+++ app/src/main/java/io/github/ava_igs/androidreactiveprogramming/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.ava_igs.androidreactiveprogramming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
